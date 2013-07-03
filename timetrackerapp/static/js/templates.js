998e4f3ee7de914c3f72dc5f38609e4779272022
agalloway <adam.galloway@nrg-edge.com>
2013-07-02 22:12:34 -0400

    added unsaved changes tracking

--- timetrackerapp/static/js/templates.js
+++ timetrackerapp/static/js/templates.js
@@ -9,18 +9,18 @@ var timeEntryRowMarkup =
 		'<input id="id_form-{{formId}}-rowId" name="form-{{formId}}-rowId" type="hidden" value="">'+
 	'</div>'+
 	'<div class="row hours-row">'+
-		'<div class="large-2 columns sun hours"><input id="id_form-{{formId}}-sundayHours" name="form-{{formId}}-sundayHours" type="text" value="0"></div>'+
-		'<div class="large-2 columns mon hours"><input id="id_form-{{formId}}-mondayHours" name="form-{{formId}}-mondayHours" type="text" value="0"></div>'+
-		'<div class="large-2 columns tue hours"><input id="id_form-{{formId}}-tuesdayHours" name="form-{{formId}}-tuesdayHours" type="text" value="0"></div>'+
-		'<div class="large-2 columns wed hours"><input id="id_form-{{formId}}-wednesdayHours" name="form-{{formId}}-wednesdayHours" type="text" value="0"></div>'+
-		'<div class="large-2 columns thu hours"><input id="id_form-{{formId}}-thursdayHours" name="form-{{formId}}-thursdayHours" type="text" value="0"></div>'+
-		'<div class="large-2 columns fri hours"><input id="id_form-{{formId}}-fridayHours" name="form-{{formId}}-fridayHours" type="text" value="0"></div>'+
-		'<div class="large-2 columns sat hours"><input id="id_form-{{formId}}-saturdayHours" name="form-{{formId}}-saturdayHours" type="text" value="0"></div>'+
+		'<div class="large-2 columns sun hours"><input id="id_form-{{formId}}-sundayHours" name="form-{{formId}}-sundayHours" data-dirty="true" type="text" value="0"></div>'+
+		'<div class="large-2 columns mon hours"><input id="id_form-{{formId}}-mondayHours" name="form-{{formId}}-mondayHours" data-dirty="true" type="text" value="0"></div>'+
+		'<div class="large-2 columns tue hours"><input id="id_form-{{formId}}-tuesdayHours" name="form-{{formId}}-tuesdayHours" data-dirty="true" type="text" value="0"></div>'+
+		'<div class="large-2 columns wed hours"><input id="id_form-{{formId}}-wednesdayHours" name="form-{{formId}}-wednesdayHours" data-dirty="true" type="text" value="0"></div>'+
+		'<div class="large-2 columns thu hours"><input id="id_form-{{formId}}-thursdayHours" name="form-{{formId}}-thursdayHours" data-dirty="true" type="text" value="0"></div>'+
+		'<div class="large-2 columns fri hours"><input id="id_form-{{formId}}-fridayHours" name="form-{{formId}}-fridayHours" data-dirty="true" type="text" value="0"></div>'+
+		'<div class="large-2 columns sat hours"><input id="id_form-{{formId}}-saturdayHours" name="form-{{formId}}-saturdayHours" data-dirty="true" type="text" value="0"></div>'+
 		'<div class="large-2 columns row-total">0.00</div>'+
 	'</div>'+
 	'<div class="row comment-row">'+
 		'<div class="large-14 columns comment">'+
-    		'<input id="id_form-{{formId}}-comment" maxlength="500" name="form-{{formId}}-comment" placeholder="Comment" type="text" value="">'+
+    		'<input id="id_form-{{formId}}-comment" maxlength="500" name="form-{{formId}}-comment" data-dirty="true" placeholder="Comment" type="text" value="">'+
     	'</div>'+
 	'</div>'+
 '</div>';
